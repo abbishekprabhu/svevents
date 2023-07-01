66d5fd144492e009740446b73677789eaa687049
abbishekprabhu <abbishekinchrist01@gmail.com>
2023-07-01 17:30:15 +0530

    meta few including pwa

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,8 +50,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `SV Events`,
+        short_name: `SV`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
